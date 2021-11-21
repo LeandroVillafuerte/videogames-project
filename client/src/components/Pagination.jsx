3ddefc997c6777f08af2deb7de8f97dd8d5f65fb
Leandro Villafuerte <leandrovillafu@gmail.com>
2021-11-21 17:03:00 -0300

    Pagination done

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,37 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-function Pagination({videogames}) {
-    // let videogames = useSelector((store)=>store.sortedvideogames);
+function Pagination({
+  currentPage,
+  setCurrentPage,
+  totalVideogames,
+  itemsPerPage,
+}) {
+  const pages = [];
+  for (let i = 1; i <= Math.ceil(totalVideogames / itemsPerPage); i++) {
+    pages.push(i);
+  }
 
-    const [currentPage,setCurrentPage] = useState(1);
-    const [itemsPerPage,setItemsPerPage]=useState(15);
-
-
-    const handleClick = (event) => {
-        setCurrentPage(Number(event.target.id))
-    }
-
-    const pages=[]
-    for (let i = 1; i < Math.ceil(videogames?.length/itemsPerPage); i++) {
-        pages.push(i)
-        
-    }
-
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = videogames?.slice(indexOfFirstItem,indexOfLastItem)
-
-    return (
-        <>
-         <ul className="pageNumbers">
-         {pages.map(number=>{
-        return(
-            <li key={number} id={number} onClick={handleClick} className={currentPage == number ? "active" : null} >{number}</li>
-        )
-        })}   
-         </ul>
-        </>
-    )
+  return (
+    <>
+      <ul className="pageNumbers">
+        {pages.map((number) => {
+          return (
+            <li
+              key={number}
+              id={number}
+              onClick={() => setCurrentPage(number)}
+              className={currentPage === number ? "active" : null}
+            >
+              <button>{number}</button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
-export default Pagination
+export default Pagination;
--- client/src/components/Videogames.jsx
+++ client/src/components/Videogames.jsx
@@ -1,29 +1,62 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchVideogames } from '../actions';
-import Videogame from './Videogame.jsx';
-import { Link } from 'react-router-dom';
-import Pagination from './Pagination.jsx';
-
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchVideogames } from "../actions";
+import Videogame from "./Videogame.jsx";
+import { Link } from "react-router-dom";
+import Pagination from "./Pagination.jsx";
 
 function Videogames() {
-    let videogames = useSelector((store)=>store.sortedvideogames);
-    let dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchVideogames())
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[]);
-    return (
-        <div>
-            <Link to='/videogame/add'><button>Add videogame</button></Link>
-            <Pagination videogames={videogames}/>
-            {videogames && videogames.length > 0?videogames.map((videogame)=>{
-            return <Videogame key= {videogame.id} genres={videogame.genres} id={videogame.id} name={videogame.name} img={videogame.background_image}/>
-            }):<span>Loading...</span>}
-        </div>
-    )
+  let dispatch = useDispatch();
+
+  let videogames = useSelector((store) => store.sortedvideogames);
+  useEffect(() => {
+    dispatch(fetchVideogames());
+  }, [dispatch]);
+
+  /*************** pagination  *****************************/
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 15;
+  // const [itemsPerPage,setItemsPerPage]=useState(15);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = videogames?.slice(indexOfFirstItem, indexOfLastItem);
+  /***************************************************** */
+
+  return (
+    <div>
+      <Link to="/videogame/add">
+        <button>Add videogame</button>
+      </Link>
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        totalVideogames={videogames.length}
+        itemsPerPage={15}
+      />
+      {currentItems && currentItems.length > 0 ? (
+        currentItems.map((videogame) => {
+          return (
+            <Videogame
+              key={videogame.id}
+              genres={videogame.genres}
+              id={videogame.id}
+              name={videogame.name}
+              img={videogame.background_image}
+            />
+          );
+        })
+      ) : (
+        <span>Loading...</span>
+      )}
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+        totalVideogames={videogames.length}
+        itemsPerPage={15}
+      />
+    </div>
+  );
 }
 
-export default Videogames
-
-
+export default Videogames;
